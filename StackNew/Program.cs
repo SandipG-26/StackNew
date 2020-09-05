ff9eae3dabb6beb65d059291d1e578b0f83ce484
SandipG-26 <70084836+SandipG-26@users.noreply.github.com>
2020-09-05 09:38:02 +0530

    Stack new with pop push and peek

---
+++ StackNew/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StackNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack meanstack = new Stack();
+            meanstack.Push(1);
+            meanstack.Push(2);
+            meanstack.Push(3);
+            meanstack.Push(4.5);
+            meanstack.Push("Hello world");
+            foreach (var item in meanstack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("count is = " + meanstack.Count);
+            meanstack.Pop();
+            foreach (var item in meanstack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("count is = " + meanstack.Count);
+            meanstack.Peek();
+            foreach (var item in meanstack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("count is = " + meanstack.Count);
+            meanstack.Clear();
+            Console.WriteLine("count is = " + meanstack.Count);
+            Console.Read();
+        }
+    }
+}
